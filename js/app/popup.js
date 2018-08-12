2f9b7b8cb8ee75d9b66fe14c6bcc4c111fa7f915
elishakay <kramer1346@gmail.com>
2018-08-12 10:54:18 +0300

    ui-router view appears

--- js/app/popup.js
+++ js/app/popup.js
@@ -1,9 +1,57 @@
 
 
-var myApp = angular.module("my-app", []);
+var myApp = angular.module("my-app", ['ui.router']);
+
+// configuring our routes 
+// =============================================================================
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        // route to show our basic form (/form)
+        .state('form', {
+            url: '/form',
+            templateUrl: '../views/form.html',
+            controller: 'formController'
+        })
+        
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: '../views/form-profile.html'
+        })
+        
+        // url will be /form/interests
+        .state('form.interests', {
+            url: '/interests',
+            templateUrl: '../views/form-interests.html'
+        })
+        
+        // url will be /form/payment
+        .state('form.payment', {
+            url: '/payment',
+            templateUrl: '../views/form-payment.html'
+        });
+       
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/profile');
+})
+
+
 
 myApp.controller("PopupCtrl", ['$scope', '$http', function($scope, $http){
    console.log("Controller Initialized");
+
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');  
+    };
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {type:"userCreds", creds:{name: "Elisha", password: "gym"}}, function(response){
@@ -12,10 +60,5 @@ myApp.controller("PopupCtrl", ['$scope', '$http', function($scope, $http){
         });
     });
 
-   // Get Background Page to get selectedText from it's scope
-   let bgPage = chrome.extension.getBackgroundPage();
-   let selectedText = bgPage.selectedText;
-   
-   
   }
 ]);
