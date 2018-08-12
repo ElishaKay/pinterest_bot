5c07620661f2d56eabbe13cbd5883f1300a64018
elishakay <kramer1346@gmail.com>
2018-08-12 23:12:07 +0300

    sending imageData from contentjs to popupjs

--- js/app/content.js
+++ js/app/content.js
@@ -46,6 +46,20 @@ if (typeof search_term != 'undefined' && search_term){
   window.location.href = 'https://www.pinterest.com/search/pins/?q='+search_term;
 }
 
+if (window.location.href.includes('?q=')){
+  let images = document.querySelectorAll('.gridCentered img');
+  let stuffToSave = [];
+  console.log('these are the images:',images);
+  for (i = 0; i < 5; i++) { 
+    let description = images[i].alt;
+    let img_src = images[i].src;
+    stuffToSave.push({description, img_src});    
+  }
+  console.log(stuffToSave);
+  chrome.runtime.sendMessage({type: "imageData", images: stuffToSave});
+}
+
+
 
 
 // if(locastorage.getItem(loggedIn) && window.location.href.includes("world")) {
